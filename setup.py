807569ccdb8586ee7380e6b8a87154d77c13ae1f
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2013-02-26 07:50:09 +0100

    Create initial project layout.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+setup(
+    name = 'virtualenv-ruby',
+    version = '0.1-dev',
+    package_dir = {
+        '': 'src',
+    },
+    packages = ['virtualenv_ruby'],
+)
