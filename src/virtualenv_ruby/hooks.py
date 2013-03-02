881f64ef34a2325029468185031ecac34a5c3bc4
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2013-03-02 09:48:53 +0100

    Extend postactivate and postdeactivate hooks.

---
+++ src/virtualenv_ruby/hooks.py
@@ -0,0 +1,70 @@
+# coding=utf-8
+
+from os.path import join
+
+_VIRTUALENV_RUBY_OLD_PATH = '_VIRTUALENV_RUBY_OLD_PATH'
+
+
+class HookBuilder(object):
+    def __init__(self, virtualenv_dir):
+        self.virtualenv_dir = virtualenv_dir
+        self._environment_vars = {}
+        self._path_extensions = []
+
+    def add_environment_variable(self, var_name, var_value):
+        self._environment_vars[var_name] = var_value
+
+    def extend_path(self, path_ext):
+        self._path_extensions.append(path_ext)
+
+    def write_hooks(self):
+        self.write_postactivate_hook()
+        self.write_postdeactivate_hook()
+
+    def write_postactivate_hook(self):
+        postactivate_hook_file = self.__get_hook_file('postactivate')
+        env_vars = self.__create_postactivate_env_var_lines()
+        path_exts = self.__create_postactivate_path_ext_lines()
+        self.__write_hook_file(postactivate_hook_file, env_vars + path_exts)
+
+    def __create_postactivate_env_var_lines(self):
+        line_tmpl = 'export {var_name}={var_value}'
+        return [
+            line_tmpl.format(var_name=n, var_value=v)
+            for n, v in self._environment_vars.items()]
+
+    def __create_postactivate_path_ext_lines(self):
+        if not self._path_extensions:
+            return []
+        path_backup = '{path_backup}=$PATH'.format(
+            path_backup=_VIRTUALENV_RUBY_OLD_PATH)
+        path_exts = 'export PATH="{paths}:$PATH"'.format(
+            paths=''.join(self._path_extensions))
+        return [path_backup, path_exts]
+
+    def write_postdeactivate_hook(self):
+        postdeactivate_hook_file = self.__get_hook_file('postdeactivate')
+        env_vars = self.__create_postdeactivate_env_var_lines()
+        path_mod = self.__create_postdeactivate_path_lines()
+        self.__write_hook_file(postdeactivate_hook_file, env_vars + path_mod)
+
+    def __create_postdeactivate_env_var_lines(self):
+        line_tmpl = 'unset {var_name}'
+        return [line_tmpl.format(var_name=n) for n in self._environment_vars]
+
+    def __create_postdeactivate_path_lines(self):
+        if not self._path_extensions:
+            return []
+        return [
+            'export PATH=${path_backup}'.format(
+                path_backup=_VIRTUALENV_RUBY_OLD_PATH),
+            'unset {path_backup}'.format(
+                path_backup=_VIRTUALENV_RUBY_OLD_PATH),
+        ]
+
+    def __get_hook_file(self, hook_name):
+        return join(self.virtualenv_dir, 'bin', hook_name)
+
+    def __write_hook_file(self, hook_file, lines):
+        with open(hook_file, 'a') as file:
+            file.writelines(lines)
---
+++ test/test_virtualenv_ruby/test_hooks.py
@@ -0,0 +1,72 @@
+# coding=utf-8
+
+from os.path import join
+from unittest import TestCase
+from mock import mock_open, patch
+
+from virtualenv_ruby import hooks
+
+
+class TestHookBuilder(TestCase):
+
+    def setUp(self):
+        self.virtual_env_dir = '/path/to/some/dir'
+        self.postactivate_hook_file = join(
+            self.virtual_env_dir, 'bin', 'postactivate')
+        self.postdeactivate_hook_file = join(
+            self.virtual_env_dir, 'bin', 'postdeactivate')
+        self.hook_builder = hooks.HookBuilder(self.virtual_env_dir)
+        self.mock_open = mock_open()
+
+    def test_extends_path_in_postactivate_hook(self):
+        path_ext = '$GEM_HOME/bin'
+        self.hook_builder.extend_path(path_ext)
+        expected_lines = [
+            '{path_backup}=$PATH'.format(
+                path_backup=hooks._VIRTUALENV_RUBY_OLD_PATH),
+            'export PATH="{path_ext}:$PATH"'.format(path_ext=path_ext),
+        ]
+        with patch('__builtin__.open', self.mock_open, create=True):
+            self.hook_builder.write_postactivate_hook()
+        self.assert_appends_to_hook(
+            self.postactivate_hook_file, expected_lines)
+
+    def test_resets_path_in_postdeactivate_hook(self):
+        expected_lines = [
+            'export PATH=${path_backup}'.format(
+                path_backup=hooks._VIRTUALENV_RUBY_OLD_PATH),
+            'unset {path_backup}'.format(
+                path_backup=hooks._VIRTUALENV_RUBY_OLD_PATH),
+        ]
+        self.hook_builder.extend_path('$GEM_HOME/bin')
+        with patch('__builtin__.open', self.mock_open, create=True):
+            self.hook_builder.write_postdeactivate_hook()
+        self.assert_appends_to_hook(
+            self.postdeactivate_hook_file, expected_lines)
+
+    def test_appends_environment_variable_to_postactivate_hook(self):
+        var_name = 'GEM_HOME'
+        var_value = '$VIRTUAL_ENV/lib/ruby/gems'
+        expected_lines = ['export ' + var_name + "=" + var_value]
+
+        self.hook_builder.add_environment_variable(var_name, var_value)
+        with patch('__builtin__.open', self.mock_open, create=True):
+            self.hook_builder.write_postactivate_hook()
+        self.assert_appends_to_hook(
+            self.postactivate_hook_file, expected_lines)
+
+    def test_appends_environment_variable_to_postdeactivate_hook(self):
+        var_name = 'GEM_HOME'
+        var_value = '$VIRTUAL_ENV/lib/ruby/gems'
+        expected_lines = ['unset ' + var_name]
+
+        self.hook_builder.add_environment_variable(var_name, var_value)
+        with patch('__builtin__.open', self.mock_open, create=True):
+            self.hook_builder.write_postdeactivate_hook()
+        self.assert_appends_to_hook(
+            self.postdeactivate_hook_file, expected_lines)
+
+    def assert_appends_to_hook(self, hook_file, expected_lines):
+        self.mock_open.assert_called_once_with(hook_file, 'a')
+        handle = self.mock_open()
+        handle.writelines.assert_called_once_with(expected_lines)
